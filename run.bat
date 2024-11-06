3c8187334633304d748d9166b16a71491ed58e62
standingmcdonalds <93478523+standingmcdonalds@users.noreply.github.com>
2024-11-06 19:49:35 +0000

    rmvcheckfleasion

--- run.bat
+++ run.bat
@@ -67,12 +67,6 @@ if not exist "%~dp0requirements.txt" curl -sSL -k -o "%~dp0requirements.txt" htt
 python -m pip install --disable-pip-version-check -r "%~dp0requirements.txt" >nul 2>&1
 goto fleasion
 
-: Just in case, check if Fleasion is there.
-:fleasion
-if exist "%~dp0fleasion.py" goto launch
-echo Downloading the latest Fleasion...
-curl -sSL -k -o "%~dp0fleasion.py" https://raw.githubusercontent.com/standingmcdonalds/Reasion/blob/main/fleasion.py --ssl-no-revoke
-
 :launch
 cd "%~dp0"
 python fleasion.py
